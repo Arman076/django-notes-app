07b15e7853f98f4d43d2d10c49f78ae0e597caad
Arman076 <132891258+Arman076@users.noreply.github.com>
2025-04-12 10:53:08 +0530

    Update settings.py
    middle ware two line added 
    'django_prometheus.middleware.PrometheusBeforeMiddleware',
        # your other middleware (e.g., security, sessions, etc.)
        'django_prometheus.middleware.PrometheusAfterMiddleware',
    
    and django installed app section changes

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
+    'django_prometheus',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -49,6 +50,9 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django_prometheus.middleware.PrometheusBeforeMiddleware',
+    # your other middleware (e.g., security, sessions, etc.)
+    'django_prometheus.middleware.PrometheusAfterMiddleware',
     #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
