575bae7875ad1e1a65cb2d5407fc0da50b811178
Arman076 <132891258+Arman076@users.noreply.github.com>
2025-04-12 10:27:57 +0530

    Update urls.py

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -13,12 +13,33 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+"""from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
-
+old url file
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
+]"""
+#ner after suggetsion 
+from django.urls import path
+from prometheus_client import generate_latest, REGISTRY
+from prometheus_client.exposition import basic_auth_handler
+from .views import *  # Your existing views
+
+# Prometheus metrics view
+def prometheus_metrics(request):
+    # This will return the metrics as a response in the Prometheus format
+    return HttpResponse(generate_latest(REGISTRY), content_type="text/plain; charset=utf-8")
+
+urlpatterns = [
+    path('', getRoutes, name="routes"),
+    path('notes/', getNotes, name="notes"),
+    path('notes/<str:pk>/update/', updateNote, name="update-note"),
+    path('notes/<str:pk>/delete/', deleteNote, name="delete-note"),
+    path('notes/create/', createNote, name="create-note"),
+    path('notes/<str:pk>/', getNote, name="note"),
+    # Add the Prometheus metrics endpoint
+    path('metrics/', prometheus_metrics, name="prometheus-metrics"),
 ]
