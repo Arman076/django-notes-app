bfd5c03bea76c950689e9d1540b8ed1862c258ed
Arman076 <132891258+Arman076@users.noreply.github.com>
2025-04-14 09:23:42 +0530

    Update urls.py

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-"""from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
 old url file
@@ -21,8 +21,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
-]"""
-#ner after suggetsion 
+]
+'''ner after suggetsion 
 from django.urls import path, include
 
 urlpatterns = [
@@ -36,3 +36,4 @@ urlpatterns = [
     # Prometheus metrics endpoint from django-prometheus
     path('', include('django_prometheus.urls')),
 ]
+'''
